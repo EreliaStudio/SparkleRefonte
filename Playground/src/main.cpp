530d15aa20d6295a334b2162d6df058c763544a7
Jeremy Boissy <jeremy.boissy@mediasofts.fr>
2024-06-28 15:05:13 +0200

    Fixing main.cpp construction

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {	
-	spk::GraphicalApplication app(L"FirstWindow", spk::Geometry2DInt(0, 0, 800, 400));
+	spk::GraphicalApplication app;
+
+	spk::SafePointer<spk::Window> firstWindow = app.createWindow(L"FirstWindow", spk::Geometry2DInt(0, 0, 800, 400));
+	spk::SafePointer<spk::Window> secondWindow = app.createWindow(L"SecondWindow", spk::Geometry2DInt(0, 0, 400, 800));
 
 	return (app.run());
 }
