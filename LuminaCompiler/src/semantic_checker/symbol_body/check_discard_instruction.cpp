eee94e390dac9b65c04a42b8fc8dcf197beee4f0
EreliaStudio <98489062+EreliaStudio@users.noreply.github.com>
2024-08-13 18:04:07 +0200

    Move the lumina project outside of the sparkle library

--- LuminaCompiler/src/semantic_checker/symbol_body/check_discard_instruction.cpp
+++
@@ -1,9 +0,0 @@
-#include "lumina_semantic_checker.hpp"
-
-namespace Lumina
-{
-	void SemanticChecker::checkDiscardInstruction(const std::filesystem::path& p_file, const std::shared_ptr<DiscardInstruction>& p_instruction, std::unordered_map<std::string, SemanticChecker::Type*> p_variables)
-	{
-		
-	}
-}
