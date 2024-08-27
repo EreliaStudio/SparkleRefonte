51f106f47b8d5623997defeda5d360b47b09e20c
EreliaStudio <98489062+EreliaStudio@users.noreply.github.com>
2024-08-28 00:48:15 +0200

    moving back the implementation inside the class, and removing the json

--- Sparkle/include/structure/graphics/opengl/spk_layout_buffer_object.hpp
+++ Sparkle/include/structure/graphics/opengl/spk_layout_buffer_object.hpp
@@ -17,116 +17,207 @@ namespace spk::OpenGL
     class LayoutBufferObject : public VertexBufferObject
     {
     public:
-        class Attribute
+        struct Attribute
         {
-        public:
             using Index = GLuint;
+
             enum class Type
             {
-                Int,
-                Float,
-                UInt,
-                Vector2,
-                Vector3,
-                Vector4,
-                Vector2Int,
-                Vector3Int,
-                Vector4Int
+				Byte = GL_BYTE,
+				UByte = GL_UNSIGNED_BYTE,
+                Int = GL_INT,
+                Float = GL_FLOAT,
+                UInt = GL_UNSIGNED_INT
             };
 
-        private:
-            Index index;
-            GLint size;
-            GLenum type;
+            Index index = 0;
+            size_t size = 0;
+			Type type = Type::Float;
 
-        public:
-            Attribute() = default;
-            Attribute(Index p_index, GLint p_size, GLenum p_type);
-            friend class LayoutBufferObject; // Allows LayoutBufferObject to access private members
+			Attribute()
+			{
+
+			}
+
+			Attribute(Index p_index, size_t p_size, Type p_type) :
+				index(p_index),
+				type(p_type),
+				size(p_size)
+			{
+
+			}
+
+			static size_t typeSize(Type p_type)
+			{
+				switch (p_type)
+				{
+				case Attribute::Type::Byte:
+					return (sizeof(GLbyte));
+				case Attribute::Type::UByte:
+					return (sizeof(GLubyte));
+				case Attribute::Type::Int:
+					return (sizeof(GLint));
+				case Attribute::Type::UInt:
+					return (sizeof(GLuint));
+				case Attribute::Type::Float:
+					return (sizeof(GLfloat));
+				}
+				return (0);
+			}
         };
 
         class Factory
         {
         private:
-            std::vector<std::tuple<GLuint, std::string, LayoutBufferObject::Attribute::Type>> layoutInfo;
+			std::vector<Attribute> _attributes;
 
         public:
-            void parse(const spk::JSON::Object& layoutJson);
-            void apply(LayoutBufferObject* layoutBuffer);
+			void insert(Attribute::Index p_index, size_t p_size, Attribute::Type p_type)
+			{
+				_attributes.push_back(Attribute(p_index, p_size, p_type));
+			}
+
+			LayoutBufferObject construct()
+			{
+				LayoutBufferObject result;
+
+				for (const auto& attribute : _attributes)
+				{
+					result._insertAttribute(attribute);
+				}
+
+				return (result);
+			}
         };
 
     private:
-        std::vector<Attribute> _attributes;
-        size_t _vertexSize = 0; // Expected vertex size
-        bool _isAttributeInitialized = false; // Tracks whether attributes have been initialized
+		friend class Factory;
+
+        std::vector<Attribute> _attributesToApply;
+        size_t _vertexSize = 0;
+
+		void _insertAttribute(const Attribute& p_attribute)
+		{
+			_attributesToApply.push_back(p_attribute);
+			_vertexSize += p_attribute.size * Attribute::typeSize(p_attribute.type);
+		}
+
+		void _applyAttributes()
+		{
+			size_t offset = 0;
+			for (const auto& attr : _attributesToApply)
+			{
+				glEnableVertexAttribArray(attr.index);
+
+				switch (attr.type)
+				{
+				case Attribute::Type::Byte:
+				case Attribute::Type::UByte:
+				case Attribute::Type::Int:
+				case Attribute::Type::UInt:
+				{
+					glVertexAttribIPointer(attr.index, attr.size, static_cast<GLenum>(attr.type), static_cast<GLsizei>(_vertexSize), reinterpret_cast<void*>(offset));
+					break;
+				}
+				case Attribute::Type::Float:
+				{
+					glVertexAttribPointer(attr.index, attr.size, static_cast<GLenum>(attr.type), GL_FALSE, static_cast<GLsizei>(_vertexSize), reinterpret_cast<void*>(offset));
+					break;
+				}
+				default:
+				{
+					throw std::runtime_error("Unexpected layout type.");
+				}
+				}
+
+				offset += attr.size * Attribute::typeSize(attr.type);
+			}
+			_attributesToApply.clear();
+		}
+
+		LayoutBufferObject() : VertexBufferObject(Type::Storage, Usage::Static)
+		{
+
+		}
 
     public:
-        LayoutBufferObject();
-        void addAttribute(Attribute::Index index, Attribute::Type attributeType);
+		LayoutBufferObject(const LayoutBufferObject& p_other) = delete;
+		LayoutBufferObject& operator = (const LayoutBufferObject& p_other) = delete;
+		
+		LayoutBufferObject(LayoutBufferObject&& p_other) noexcept :
+			VertexBufferObject(std::move(p_other)),
+			_attributesToApply(std::move(p_other._attributesToApply)),
+			_vertexSize(p_other._vertexSize)
+		{
+			p_other._vertexSize = 0;
+		}
 
-        virtual void activate() override;
+		LayoutBufferObject& operator = (LayoutBufferObject&& p_other) noexcept
+		{
+			if (this != &p_other)
+			{
+				VertexBufferObject::operator=(std::move(p_other));
 
-        template<typename TType>
-        void append(const TType& data)
-        {
-            // Check the size of TType
-            checkVertexSize<TType>();
-            VertexBufferObject::append(&data, sizeof(TType));
-        }
+				_attributesToApply = std::move(p_other._attributesToApply);
+				_vertexSize = p_other._vertexSize;
 
-        template<typename TType>
-        void append(const std::vector<TType>& data)
-        {
-            // Check the size of TType
-            checkVertexSize<TType>();
-            VertexBufferObject::append(data.data(), data.size() * sizeof(TType));
-        }
+				p_other._vertexSize = 0;
+			}
+			return *this;
+		}
 
-        template<typename TType>
-        void append(const std::span<TType>& data)
-        {
-            // Check the size of TType
-            checkVertexSize<TType>();
-            VertexBufferObject::append(data.data(), data.size() * sizeof(TType));
-        }
+        virtual void activate() override
+		{
+			VertexBufferObject::activate();
+			if (_attributesToApply.empty() == false)
+			{
+				_applyAttributes();
+			}
+		}
 
         template<typename TType>
         LayoutBufferObject& operator<<(const TType& data)
         {
-            append(data);
+			append(std::span<const TType>(&data, 1));
             return *this;
         }
 
         template<typename TType>
         LayoutBufferObject& operator<<(const std::vector<TType>& data)
         {
-            append(data);
+			append(std::span<const TType>(data));
             return *this;
         }
 
         template<typename TType>
         LayoutBufferObject& operator<<(const std::span<TType>& data)
         {
-            append(data);
+			append(data);
             return *this;
         }
 
-    private:
-        void _applyAttributes();
+		template<typename TType>
+		void append(const TType& data)
+		{
+			append(std::span<const TType>(&data, 1));
+		}
 
-        size_t _typeSize(GLenum type) const;
+		template<typename TType>
+		void append(const std::vector<TType>& data)
+		{
+			append(std::span<const TType>(data));
+		}
 
-        template<typename TType>
-        void checkVertexSize() const
-        {
-            if (sizeof(TType) != _vertexSize)
-            {
-                throw std::runtime_error("Size mismatch inside LayoutBufferObject: Expected vertex size is " +
-                    std::to_string(_vertexSize) + " bytes, but received " +
-                    std::to_string(sizeof(TType)) + " bytes.");
-            }
-        }
+		template<typename TType>
+		void append(const std::span<TType>& data)
+		{
+			if (sizeof(TType) != _vertexSize)
+			{
+				throw std::runtime_error("Size mismatch inside LayoutBufferObject: Expected vertex size is " +
+					std::to_string(_vertexSize) + " bytes, but received " +
+					std::to_string(sizeof(TType)) + " bytes.");
+			}
+			VertexBufferObject::append(data.data(), data.size() * sizeof(TType));
+		}
     };
-
-    LayoutBufferObject::Attribute::Type wstringToAttributeType(const std::wstring& p_value);
 }
